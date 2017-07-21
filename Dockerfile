c6a6944562d4db80b90ac7e8808ec1adcb4cfb17
root <root@ip-172-18-0-233.us-west-2.compute.internal>
2017-07-21 08:21:12 +0000

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM alpine:3.6
 MAINTAINER Aviad Pines (Forked from Calvin Leung Huang <https://github.com/calvn>)
 
-ADD start.sh .
+ADD start.sh /start.sh
+RUN chmod +x /start.sh
 
-RUN apk --update add nodejs nodejs-npm git openssh ca-certificates && \
+RUN apk --update add nodejs nodejs-npm git openssh ca-certificates openssl && \
     rm -rf /var/cache/apk/* && \
     npm install git2consul@0.12.13 --global && \
     mkdir -p /etc/git2consul.d
 
-CMD start.sh
+#ENTRYPOINT [ "sh", "-c", "start.sh" ]
+CMD [ "/start.sh" ]
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!bin/sh
 
 if [ -z "$CONSUL_IP" ] || [ -z "$CONFIG_URI" ] ; then
-    echo "CONSUL_IP and CONFIG_URI must be defined
+    echo "CONSUL_IP and CONFIG_URI must be defined"
     exit 1
 fi
 
@@ -9,4 +9,8 @@ if [ ! -z $CONFIG_URI ]; then
     wget -O /etc/git2consul.d/config.json $CONFIG_URI 
 fi
 
-/usr/lib/node_modules/git2consul --endpoint $CONSUL_IP --port ${CONSUL_PORT:=8500} --config-file /etc/git2consul.d/config.json
+#/bin/git2consul --endpoint $CONSUL_IP --port ${CONSUL_PORT:=8500} --config-file /etc/git2consul.d/config.json
+/usr/bin/git2consul --endpoint $CONSUL_IP --port ${CONSUL_PORT:=8500} --config-file /etc/git2consul.d/config.json
+
+
+#while true; do sleep 60; done
