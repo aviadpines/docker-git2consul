c72babbdef876c0e7a8685b3bf486b8c00487477
Aviad <aviadpines@gmail.com>
2017-07-21 14:17:50 +0300

    config key

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!bin/sh
 
-if [ -z "$CONSUL_IP" ] || [ -z "$CONFIG_URI" ] ; then
-    echo "CONSUL_IP and CONFIG_URI must be defined"
+if [ -z "$CONSUL_IP" ] || [ -z "$CONFIG_URI" ] || [ -z "$CONFIG_KEY" ]; then
+    echo "CONSUL_IP, CONFIG_URI and CONFIG_KEY must be defined"
     exit 1
 fi
 
-if [ ! -z $CONFIG_URI ]; then
-    wget -O /etc/git2consul.d/config.json $CONFIG_URI 
-fi
+wget -O /etc/git2consul.d/config.json $CONFIG_URI 
 
-/usr/bin/git2consul --endpoint $CONSUL_IP --port ${CONSUL_PORT:=8500} --config-file /etc/git2consul.d/config.json
+/usr/bin/git2consul \
+    --endpoint $CONSUL_IP \
+    --port ${CONSUL_PORT:=8500} \
+    --config-file /etc/git2consul.d/config.json \
+    --config-key git2consul/${CONFIG_KEY}/config
